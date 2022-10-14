a6a768ee001203ca6c88ff01edac191b05bece07
Thomas Hetzel <tmhetz@gmail.com>
2022-10-14 16:47:05 -0500

    Create a function to select computer choice
    The function getComputerChoice was created and returns the computer's
    game selection at random.

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random()*3)+1;
+    if(randomNumber === 1){
+        return 'rock';
+    } else if(randomNumber === 2){
+        return 'paper';
+    }
+    return 'scissors';
+}
